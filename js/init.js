711cb92d518f71678bc80379c79ff17d8bcd3d50
Luana <luana_limas@hotmail.com>
2020-01-22 21:52:40 -0300

    Adicionando firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCuhERd1iLaEW99K2qsJSabtTkTc9GKkLQ",
+    authDomain: "linkme-dba8e.firebaseapp.com",
+    databaseURL: "https://linkme-dba8e.firebaseio.com",
+    projectId: "linkme-dba8e",
+    storageBucket: "linkme-dba8e.appspot.com",
+    messagingSenderId: "661312273506",
+    appId: "1:661312273506:web:49c87e97219323d9a4653b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
